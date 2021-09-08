ad7946f1c7466f7443906063b80f9c992b6ab8ef
pedro lima <pelyma33@gmail.com>
2021-09-08 11:40:48 -0300

    criando repositorio no github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'course-carsales';
+  title = 'Bem Vindo ao Curso FrontEnd Angular'
 }
